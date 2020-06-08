b1fbd2c68cec4d229011ef59279521a754380b26
sambuddha92 <sambuddhaadhikari@gmail.com>
2020-06-09 02:11:50 +0530

    my acc

--- routes/api/resource.js
+++ routes/api/resource.js
@@ -18,11 +18,11 @@ const Course = require('../../models/course.js');
 const Teacher = require('../../models/teacher.js');
 const Lesson = require('../../models/lesson.js');
 
-//@route    GET api/page/mypage
-//@desc     Get All Courses
+//@route    GET api/page/my-wingmait
+//@desc     Get MyWingmait Resource
 //@access   private
 
-router.get('/mypage', async (req, res) => {
+router.get('/my-wingmait', async (req, res) => {
     try {
         if (req.isAuthenticated()) {
             let response = {
@@ -32,6 +32,40 @@ router.get('/mypage', async (req, res) => {
                     name: req.user.name,
                     enrolments: req.user.enrolments,
                     payments: req.user.payments
+                }
+            }
+            return res.status(200).json(response);
+        } else {
+            let response = {
+                success: false,
+                msg: "Unauthorized"
+            }
+            return res.status(401).json(response)
+        }
+    } catch (err) {
+        let response = {
+            success: false,
+            msg: "Server error",
+            details: "An unexpected error occured while getting mypage",
+            err
+        }
+        res.status(500).json(response);
+    }
+})
+
+//@route    GET api/page/my-account
+//@desc     Get MyAccount Resource
+//@access   private
+
+router.get('/my-account', async (req, res) => {
+    try {
+        if (req.isAuthenticated()) {
+            let response = {
+                success: true,
+                msg: "My Page Data",
+                payload: {
+                    name: req.user.name,
+                    email: req.user.email
                 }
             }
             return res.status(200).json(response);
